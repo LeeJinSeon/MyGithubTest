3db9dc95a77a5c0a59efecf736620d21f8abf120
LeeJinSeon <seon985@gmail.com>
2020-02-10 10:30:08 +0900

    수정된 내용입니다.

--- app/src/main/java/com/ezen/mygiyhubtest/MainActivity.java
+++ app/src/main/java/com/ezen/mygiyhubtest/MainActivity.java
@@ -9,6 +9,8 @@ public class MainActivity extends AppCompatActivity {
     //https://git-scm.com/ 가서 다운로드 -> 윈도우
     // 깃허브 네임 LeeJinSeon / 아이디 쥐메일 / 비번 1.
 
+    // 소스를 수정했습니다.
+
    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
