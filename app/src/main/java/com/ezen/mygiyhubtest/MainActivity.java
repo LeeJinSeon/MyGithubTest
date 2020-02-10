a9339513c51969dc089f7998a33018e7fea9d524
LeeJinSeon <seon985@gmail.com>
2020-02-10 10:26:14 +0900

    초기버전

---
+++ app/src/main/java/com/ezen/mygiyhubtest/MainActivity.java
@@ -0,0 +1,17 @@
+package com.ezen.mygiyhubtest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    //https://git-scm.com/ 가서 다운로드 -> 윈도우
+    // 깃허브 네임 LeeJinSeon / 아이디 쥐메일 / 비번 1.
+
+   @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
